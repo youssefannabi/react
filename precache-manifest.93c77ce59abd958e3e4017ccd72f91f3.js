070e60ae4abc6ff2eb7ff6679dff78188428f4c0
cat <youssefannabi@gmail.com>
2020-07-15 10:00:48 +0100

    Updates

---
+++ precache-manifest.93c77ce59abd958e3e4017ccd72f91f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17d425a61ef105f44b94a91b5549df2a",
+    "url": "/check3/index.html"
+  },
+  {
+    "revision": "1c6fcc4a38b72c130d8b",
+    "url": "/check3/static/css/main.4f19b778.chunk.css"
+  },
+  {
+    "revision": "4104f5ddb6094e085847",
+    "url": "/check3/static/js/2.a60e9264.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/check3/static/js/2.a60e9264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6fcc4a38b72c130d8b",
+    "url": "/check3/static/js/main.b2b165bd.chunk.js"
+  },
+  {
+    "revision": "0702d30d4326a20dc2d4",
+    "url": "/check3/static/js/runtime-main.6ba05889.js"
+  }
+]);
